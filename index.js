259a4f6cd5b2313d04eb90c839ad4bb850572805
nhannguyen1122 <52160217+nhannguyen1122@users.noreply.github.com>
2020-10-08 13:32:10 +0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express=require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const userlogin=require("./route");
 let playlist=require("./route/playlistroute");
 const app=express();
+app.use(cors())
 const userverify=require("./validation/playlistverify");
 const bodyParser = require('body-parser');
 mongoose.connect(process.env.db_connection, {useNewUrlParser: true,useUnifiedTopology: true });
